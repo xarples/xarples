add150b0bfcf16601096d92cb0222ebd9de4c176
Guillermo <guillermo@mantl.com>
2020-02-05 17:25:55 -0400

    fix: web host

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,14 +1,14 @@
 import { Configuration } from '@nuxt/types'
 import xarplesConfig from '@xarples/config'
 
-const { host, port } = xarplesConfig.web.service
+const { port } = xarplesConfig.web.service
 
 const config: Configuration = {
   buildModules: ['@nuxt/typescript-build'],
   css: ['~/assets/main.css'],
   modules: ['nuxt-buefy'],
   srcDir: './src',
-  server: { host, port },
+  server: { port },
   typescript: {
     typeCheck: {
       eslint: process.env.NODE_ENV === 'development'
